0ef7346ad02c276ce489b93acd623e9da9a8529e
Trevis McGill <trevismcgill25@gmail.com>
2020-07-31 04:58:57 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,14 +5,14 @@ def second_challenge
       matriarch: {},
       hero: {},
       hero_friends: []
-      }, 
+      },
     :capulet => {
       patriarch: {},
       matriarch: {},
       heroine: {},
       heroine_friends: []
-      
+
       }}
 
-  
+
 end
