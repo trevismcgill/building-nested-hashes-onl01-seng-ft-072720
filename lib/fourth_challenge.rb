e0c84b23b518f99b34031044bc0229b28f84b732
Trevis McGill <trevismcgill25@gmail.com>
2020-07-31 05:20:21 +0000

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,18 +6,38 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
+        hero_f1 = {
+          name: "Benvolio",
+          age: "17",
+          attitude: "worried"
+        },
+        hero_f2 = {
+          name: "Mercutio",
+          age: "18",
+          attitude: "hot-headed"
+        }
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        heroine_f1 = {
+          name: "Steven",
+          age: "30",
+          attitude: "confused"
+        },
+        heroine_f2 = {
+          name: "Nurse",
+          age: "44",
+          attitude: "worried"
+        }
       ]
    }
 }
 
-  
+
 
 end
